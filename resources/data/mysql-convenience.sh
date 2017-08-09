9bd6076f6dfded18d331a0a0ad5768f15705990a
Bharat Patel <b007patel@yahoo.com>
2017-08-08 20:10:14 -0400

    - removed DB credentials from convenience test DB setup script

--- resources/data/mysql-convenience.sh
+++ resources/data/mysql-convenience.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-mysql -u root --password=beerbeer < ~/gitrepo/tutor-prez/resources/data/mysql/test_data_schema.sql 
-mysql -u root --password=beerbeer < ~/gitrepo/tutor-prez/resources/data/mysql/test_data.sql
+# change __admin*__ to appropriate values
+
+mysql_script_dir=~/gitrepo/tutor-prez/resources/data/mysql/
+pushd ${mysql_script_dir}
+
+mysql -u __adminuser__ --password=__adminpwd__ < ./test_data_schema.sql
+mysql -u __adminuser__ --password=__adminpwd__ < ./test_data.sql
+
+popd
